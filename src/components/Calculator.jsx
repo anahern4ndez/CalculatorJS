b49c2416c726409f31ce8ca839929d87f7bc8233
Ana Hernandez <her17138@uvg.edu.gt>
2019-10-09 14:38:04 -0600

    funcionalidad terminada

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,12 +3,15 @@ import React, { Component } from "react";
 import Number from "./Number.jsx";
 import Display from "./Display.jsx";
 import Operation from "./Operation.jsx";
+import  "../styles/style.css";
+//import { stat } from "fs";
 
 class Calculator extends Component {
     constructor(props){
         super(props);
         this.state = {
-            storedDisplay: "0",
+            op1: "0",
+            op2: "0",
             currentOperation: null,
             display: "",
         }
@@ -20,8 +23,8 @@ class Calculator extends Component {
         for (let index = 0; index < 10; index++) {
             buttons.push(<Number key={index} nvalue={index} handleClick={this.onNumberClick.bind(this, index)}/>)    
         }
-        const operations = ["+", "-", "/", "*", "-", "%"]
-        for (let i = 0; i < 5; i++) {
+        const operations = ["+", "-", "/", "*", "=", "%"]
+        for (let i = 0; i < 6; i++) {
             buttons.push(<Operation value={operations[i]} key={i+10} handleClick={this.onOperationClick.bind(this, operations[i])}/>)
         }
         return buttons
@@ -29,9 +32,21 @@ class Calculator extends Component {
 
     onNumberClick(value){
         //const newText = display.concat(value)
-        if(this.state.display.length < 9){
+        if(this.state.display.length < 9 && this.state.currentOperation === null){
             this.setState(state => ({
-                display: state.display.concat(value)
+                display: state.display.concat(value),
+                op1: state.op1.concat(value)
+            }))
+        }
+        else if (this.state.currentOperation !== null && this.state.op2 === "0"){
+            this.setState(state => ({
+                display: "".concat(value),
+                op2: "".concat(value)
+            }))
+        }else if (this.state.display.length < 9 && this.state.currentOperation !== null && this.state.op2 !== "0"){
+            this.setState(state => ({
+                display: state.display.concat(value),
+                op2: state.op2.concat(value)
             }))
         }
     }
@@ -48,20 +63,21 @@ class Calculator extends Component {
         else if(type === "-"){
             result = n1-n2
         }*/
-        if (this.state.currentOperation !== null && this.state.storedDisplay !== "0") {
+        if (this.state.currentOperation !== null && this.state.op2 !== "0") {
             this.calculate()
         }
         this.setState(state=> ({
-            storedDisplay: state.display,
+            //storedDisplay: state.display,
             //display: "0",
-            currentOperation: type
+            currentOperation: type,
         }))
     }
     calculate(){
         let result = 0 
-        const n1 = parseFloat(this.state.storedDisplay, 10)
-        const n2 = parseFloat(this.state.display,10)
+        const n1 = parseFloat(this.state.op1, 10)
+        const n2 = parseFloat(this.state.op2, 10)
         const op = this.state.currentOperation
+        console.log("n1 " + n1 + " n2 " + n2 + " op " + op)
         if(op === "+"){
             result = n1+n2
         }
@@ -75,8 +91,9 @@ class Calculator extends Component {
             result = n1-n2
         }
         this.setState( state => ({
-            storedResult: state.display,
-            display: result.toString()
+            display: result.toString(),
+            op1: result.toString(),
+            op2: "0"
         }))
 
     }
